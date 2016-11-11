b52a2ecd3f3428681675252caccb031f91eb22c1
Andrew Dolman <andydolman@gmail.com>
2016-11-11 15:59:54 +0100

    tidy Subsample documentation

--- R/SubsampleTimeseries_MgCa.R
+++ R/SubsampleTimeseries_MgCa.R
@@ -1,9 +1,9 @@
-#' SubsampleTimeseries_MgCa
-#'
-#' @param tss a pTs object containing an equidistant timeseries, (e.g. model
-#'   result or interpolated time series), units = kyr
+#' @title SubsampleTimeseries_MgCa
+#' @name SubsampleTimeseries_MgCa
+#' @param tss a ts object containing an equidistant timeseries, (e.g. model
+#'   result or interpolated time series)
 #' @param timepoints the timepoints at which to sample the time series
-#' @param biowidth.time width of the averaging window in years
+#' @param biowidth.time width of the averaging window in no. of time points
 #' @param NForam Number of foraminifera sampled per timepoint
 #' @description Resamples an equidistant sampled timeseries (e.g. model result)
 #'   at the specified "timepoints" by simulating the foraminifera sampling
@@ -27,57 +27,57 @@
 #' plot(tss)
 #' lines(data~time, data = result, col = "Red")
 
-SubsampleTimeseries_MgCa <-
-  function(tss,
-           timepoints,
-           biowidth.time,
-           NForam = 30)
+SubsampleTimeseries_MgCa <- function(tss,
+                                     timepoints,
+                                     biowidth.time,
+                                     NForam = 30)
+{
+  time_step <- 1 / frequency(tss)
+  #width of the transfer function
+  #(compromise 4x biowidth contains most of the area of the impulse response)
+  z <- seq(
+    from = -1 * biowidth.time,
+    to = 3 * biowidth.time,
+    by = 1
+  )
+  #impulse response in year units; use inverted z
+  weights <-
+    ImpulseResponse(-z, biowidth.time, z0 = 0)
 
+  #Go in the index space; index.time contains the indices of the timepoints as index# in tss
+  index.time <- (timepoints - c(time(tss[1])) + 1) / time_step
+  #Only use points inside the timeseries
+  index.time <-
+    index.time[index.time > 0]
+  N.tss <- length(tss) #length of the timeseries
+  index.time <- index.time[index.time <= N.tss]
+
+  tss <- c(tss) #remove timeseries attributes
+
+
+  index.time0 <-
+    (biowidth.time + 1) #Index of the weight which corresponds to time 0
+
+  result <- list()
+  result$data <- rep(NA, length(timepoints))
+  for (i in 1:length(index.time))
   {
-    time_step <- 1/frequency(tss)
-    #width of the transfer function
-    #(compromise 4x biowidth contains most of the area of the impulse response)
-    z <- seq(from = -1 * biowidth.time,
-             to = 3 * biowidth.time,
-             by = 1)
-    #impulse response in year units; use inverted z
-    weights <-
-      ImpulseResponse(-z, biowidth.time, z0 = 0)
+    shift <-
+      index.time[i] - index.time0
+    #Shift of the weights that zero time overlaps with the sampling time
 
-    #Go in the index space; index.time contains the indices of the timepoints as index# in tss
-    index.time <- (timepoints - c(time(tss[1]))+1) / time_step
-    #Only use points inside the timeseries
-    index.time <-
-      index.time[index.time > 0]
-    N.tss <- length(tss) #length of the timeseries
-    index.time <- index.time[index.time <= N.tss]
+    #Put weights outside of the timeseries to zero
+    weights.cut <- weights
+    index.weight.shift <-
+      (1:length(weights)) + shift #index to simplify the
+    weights.cut[(index.weight.shift < 1)] <- 0
+    weights.cut[(index.weight.shift) > N.tss] <- 0
 
-    tss <- c(tss) #remove timeseries attributes
-
-
-     index.time0 <-
-       (biowidth.time + 1) #Index of the weight which corresponds to time 0
-
-    result <- list()
-    result$data <- rep(NA, length(timepoints))
-    for (i in 1:length(index.time))
-    {
-      shift <-
-        index.time[i] - index.time0
-      #Shift of the weights that zero time overlaps with the sampling time
-
-      #Put weights outside of the timeseries to zero
-      weights.cut <- weights
-      index.weight.shift <-
-        (1:length(weights)) + shift #index to simplify the
-      weights.cut[(index.weight.shift < 1)] <- 0
-      weights.cut[(index.weight.shift) > N.tss] <- 0
-
-      #create random numbers with the probabilities of the weights
-      rIndex <- rWeights(NForam, weights.cut) + shift
-      #print(rIndex)
-      result$data[i] <- mean(tss[rIndex])
-    }
-    result$time <- timepoints
-    return(result)
+    #create random numbers with the probabilities of the weights
+    rIndex <- rWeights(NForam, weights.cut) + shift
+    #print(rIndex)
+    result$data[i] <- mean(tss[rIndex])
   }
+  result$time <- timepoints
+  return(result)
+}
