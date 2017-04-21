bec60f192a7059e8cf9342647d46dd197f812a70
Andrew Dolman <andydolman@gmail.com>
2017-04-21 17:36:20 +0200

    work on manuscript

---
+++ R/sedproxy.R
@@ -0,0 +1,7 @@
+#' sedproxy: Simulation of Sediment Archived Climate Proxy Records
+#'
+#' The sedproxy package provides functions to simulate sediment archived proxies
+#' @importFrom limnolrgy gg_arrange
+#' @docType package
+#' @name sedproxy
+NULL
