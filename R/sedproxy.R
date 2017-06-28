0d2d6eb4d32bd53fbedb4ba9f0e3ed2fc4db37b4
Andrew Dolman <andydolman@gmail.com>
2017-06-28 17:35:30 +0200

    remove limnolrgy dependency

--- R/sedproxy.R
+++ R/sedproxy.R
@@ -1,7 +1,6 @@
 #' sedproxy: Simulation of Sediment Archived Climate Proxy Records
 #'
 #' The sedproxy package provides functions to simulate sediment archived proxies
-#' @importFrom limnolrgy gg_arrange
 #' @docType package
 #' @name sedproxy
 NULL
