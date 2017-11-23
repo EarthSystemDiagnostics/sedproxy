5141ed8efb7538abac7bdba12e60c2981be809a7
Andrew Dolman <andydolman@gmail.com>
2017-11-23 10:48:58 +0100

    import taxa temperature preference parameters and create function to return abundance/productivity as a function of temperature.

---
+++ data-raw/import-temperature-parameters.R
@@ -0,0 +1,8 @@
+# import temperature preference parameters
+library(tidyverse)
+taxa.temperature.prefs <- readr::read_delim("data-raw/temperature-parameters.csv", delim = ";") %>%
+  dplyr::mutate(Tmin = ifelse(is.na(Tmin), -Inf, Tmin),
+                Tmax = ifelse(is.na(Tmax), Inf, Tmax),
+                ID = paste(Taxon, Source, sep = " "))
+
+devtools::use_data(taxa.temperature.prefs, overwrite = T)
