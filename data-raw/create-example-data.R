8480d51d048c60bcc97d99cf4e6ccbc0ab9c21d2
Andrew Dolman <andydolman@gmail.com>
2017-12-19 13:19:25 +0100

    remove annual sed rate from example data

--- data-raw/create-example-data.R
+++ data-raw/create-example-data.R
@@ -13,7 +13,7 @@ N41.proxy <- climproxyrecords::shakun.sed.acc %>%
                                         0.2 * mean(Sed.acc.rate.m.yr),
                                         Sed.acc.rate.m.yr),
          Sed.acc.rate.cm.kyr = round(Sed.acc.rate.m.yr * 1000*100, 2)) %>%
-  select(Published.age, Published.temperature, Sed.acc.rate.m.yr, Sed.acc.rate.cm.kyr)
+  select(Published.age, Published.temperature, Sed.acc.rate.cm.kyr)
 
 N41.proxy.details <- climproxyrecords::shakun.metadata %>%
   filter(ID.no == "N41")
