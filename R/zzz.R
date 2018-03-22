1dc70826b038c61f6b6ddecf48313a8f92de283a
Andrew Dolman <andydolman@gmail.com>
2018-03-22 14:11:03 +0100

    add import for readr

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,13 @@
 # Create self contained Shiny app by pasting together
 # the required functions and the ui and server files.
-library(readr)
 obj <- paste0(
   "library(ggplot2) \n",
-  read_file("R/ClimToProxyClim.R"),
-  read_file("R/PlotPFMs.R"),
-  read_file("R/BioturbationWeights.R"),
-  read_file("inst/ui-and-server.R"),
+  readr::read_file("R/ClimToProxyClim.R"),
+  readr::read_file("R/PlotPFMs.R"),
+  readr::read_file("R/BioturbationWeights.R"),
+  readr::read_file("inst/ui-and-server.R"),
   sep = "\n",
   collapse = "\n"
 )
-write_file(obj, path = "inst/sedproxy-shiny/app.R")
+readr::write_file(obj, path = "inst/sedproxy-shiny/app.R")
 
