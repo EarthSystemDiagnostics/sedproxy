4401c0d3143989ad918209fe750c4e2efba7bd92
Andrew Dolman <andydolman@gmail.com>
2018-06-14 16:33:56 +0200

    change production bias to habitat bias

--- inst/ui-and-server.R
+++ inst/ui-and-server.R
@@ -1,32 +1,89 @@
 # Objects
-stages.key <- structure(list(stage = c("timepoints", "clim.signal.ann", "clim.signal.smoothed",
-                         "clim.timepoints.ssr", "proxy.bt", "proxy.bt.sb", "proxy.bt.sb.inf.b",
-                         "proxy.bt.sb.inf.b.n", "proxy.bt.sb.sampY", "proxy.bt.sb.sampYM",
-                         "proxy.bt.sb.sampYM.b", "proxy.bt.sb.sampYM.b.n", "simulated.proxy",
-                         "observed.proxy"), label = c("Requested timepoints", "(1) Input climate",
-                                                      "(1) Input climate", "(1) Input climate", "(2) +Bioturbation",
-                                                      "(3) +Production bias", "(.) +Calibration bias", "(5) +Measurement error",
-                                                      "(4) +Aliasing Y", "(4) +Aliasing YM", "(.) +Calibration bias",
-                                                      "(5) +Measurement error", "(5) Simulated proxy", "(*) Observed proxy"
-                         ), description = c("Requested timepoints", "Input climate signal at requested timepoints at annual resolution",
-                                            "Input climate signal at regular time intervals and resolution = smoothed.signal.res",
-                                            "Input climate signal at requested timepoints, smoothed to resolution = smoothed.signal.res",
-                                            "Climate signal after bioturbation", "Climate signal after bioturbation and production bias",
-                                            "Climate signal after bioturbation, production bias, and calibration bias",
-                                            "Climate signal after bioturbation, production bias, and measurement error",
-                                            "Climate signal after bioturbation, production bias, and aliasing of inter-annual variation",
-                                            "Climate signal after bioturbation, production bias, and aliasing of inter-annual and intra-annual variation such as monthly temperatures or depth habitats",
-                                            "Climate signal after bioturbation, production bias, and aliasing of inter-annual and intra-annual variation such as monthly temperatures or depth habitats, and calibration bias",
-                                            "Climate signal after bioturbation, production bias, aliasing, and measurement error",
-                                            "Final simulated pseudo-proxy, this will be same as proxy.bt.sb.inf.b.n when n.samples = Inf, and proxy.bt.sb.sampYM.b.n when n.samples is finite",
-                                            "True observed proxy (when supplied)"), plot.order = c(1, 1,
-                                                                                                   2, 3, 4, 5, 10, 6, 7, 8, 9, 11, 12, 13), plotting.colour = c("Black",
-                                                                                                                                                                "#018571", "#018571", "#018571", "Green", "Gold", "Pink", "#7570b3",
-                                                                                                                                                                "#d95f02", "#d95f02", "Pink", "#7570b3", "#7570b3", "Red"), plotting.alpha = c(1,
-                                                                                                                                                                                                                                               1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)), .Names = c("stage",
-                                                                                                                                                                                                                                                                                                               "label", "description", "plot.order", "plotting.colour", "plotting.alpha"
-                                                                                                                                                                                                                                               ), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA,
-                                                                                                                                                                                                                                                                                                          -14L))
+stages.key <-
+  structure(
+    list(
+      stage = c(
+        "timepoints",
+        "clim.signal.ann",
+        "clim.signal.smoothed",
+        "clim.timepoints.ssr",
+        "proxy.bt",
+        "proxy.bt.sb",
+        "proxy.bt.sb.inf.b",
+        "proxy.bt.sb.inf.b.n",
+        "proxy.bt.sb.sampY",
+        "proxy.bt.sb.sampYM",
+        "proxy.bt.sb.sampYM.b",
+        "proxy.bt.sb.sampYM.b.n",
+        "simulated.proxy",
+        "reconstructed.climate",
+        "observed.proxy"
+      ),
+      label = c(
+        "Requested timepoints",
+        "(1) Input climate",
+        "(1) Input climate",
+        "(1) Input climate",
+        "(2) +Bioturbation",
+        "(3) +Habitat bias",
+        "(.) +Calibration bias",
+        "(5) +Measurement error",
+        "(4) +Aliasing Y",
+        "(4) +Aliasing YM",
+        "(.) +Calibration bias",
+        "(5) +Measurement error",
+        "(5) Simulated proxy",
+        "(6) Reconstructed climate",
+        "(*) Observed proxy"
+      ),
+      description = c(
+        "Requested timepoints",
+        "Input climate signal at requested timepoints at annual resolution",
+        "Input climate signal at regular time intervals and resolution = smoothed.signal.res",
+        "Input climate signal at requested timepoints, smoothed to resolution = smoothed.signal.res",
+        "Climate signal after bioturbation",
+        "Climate signal after bioturbation and habitat bias",
+        "Climate signal after bioturbation, habitat bias, and calibration bias",
+        "Climate signal after bioturbation, habitat bias, and measurement error",
+        "Climate signal after bioturbation, habitat bias, and aliasing of inter-annual variation",
+        "Climate signal after bioturbation, habitat bias, and aliasing of inter-annual and intra-annual variation such as monthly temperatures or depth habitats",
+        "Climate signal after bioturbation, habitat bias, and aliasing of inter-annual and intra-annual variation such as monthly temperatures or depth habitats, and calibration bias",
+        "Climate signal after bioturbation, habitat bias, aliasing, and measurement error",
+        "Final simulated pseudo-proxy, this will be same as proxy.bt.sb.inf.b.n when n.samples = Inf, and proxy.bt.sb.sampYM.b.n when n.samples is finite",
+        "True observed proxy (when supplied)",
+        "Final pseudo-proxy calibrated to temperature"
+      ),
+      plot.order = c(1, 1, 2, 3, 4, 5, 10, 6, 7, 8, 9, 11, 12, 13,
+                     14),
+      plotting.colour = c(
+        "Black",
+        "#018571",
+        "#018571",
+        "#018571",
+        "Green",
+        "Gold",
+        "Pink",
+        "#7570b3",
+        "#d95f02",
+        "#d95f02",
+        "Pink",
+        "#7570b3",
+        "#7570b3",
+        "Blue",
+        "Red"
+      ),
+      plotting.alpha = c(1, 1,
+                         1, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
+    ),
+    .Names = c(
+      "stage",
+      "label",
+      "description",
+      "plot.order",
+      "plotting.colour",
+      "plotting.alpha"
+    ), row.names = c(NA, -15L), class = c("tbl_df", "tbl", "data.frame"
+                                                                                                                                                                                                 ))                                                                                                            "#d95f02", "#d95f02", "Pink", "#7570b3", "#7570b3", "Red"), plotting.alpha = c(1,
 
 # Functions
 SimPowerlaw <- function(beta, N)
