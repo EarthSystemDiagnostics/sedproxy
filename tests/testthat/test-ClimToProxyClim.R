b5a6709aa423fdbfa76eaf19de0274e57871d6fe
andrewdolman <andydolman@gmail.com>
2021-03-19 21:50:45 +0100

    add tests for mixed layer

--- tests/testthat/test-ClimToProxyClim.R
+++ tests/testthat/test-ClimToProxyClim.R
@@ -226,39 +226,46 @@ test_that("meas.bias applied repwise", {
   })
 
 
-# test_that("First rep equal to single rep"){
-#
-#   clim.in <- N41.t21k.climate[nrow(N41.t21k.climate):1,] - 273.15
-#   clim.in <- ts(clim.in, start = -39)
-#
-#   tpts <- round(seq(4000, 20000, length.out = 10))
-#
-#   set.seed(26052017)
-#   PFM.slow.1 <- ClimToProxyClim(clim.signal = clim.in,
-#                                 timepoints = tpts,
-#                                 habitat.weights = N41.G.ruber.seasonality,
-#                                 sed.acc.rate = rep(50, length(tpts)),
-#                                 layer.width = 0,
-#                                 sigma.meas = 0.46,
-#                                 sigma.ind = 0,
-#                                 n.samples = 30,
-#                                 n.replicates = 1)
-#   set.seed(26052017)
-#   PFM.slow.2 <- ClimToProxyClim(clim.signal = clim.in,
-#                                 timepoints = tpts,
-#                                 habitat.weights = N41.G.ruber.seasonality,
-#                                 sed.acc.rate = rep(50, length(tpts)),
-#                                 layer.width = 0,
-#                                 sigma.meas = 0.46,
-#                                 sigma.ind = 0,
-#                                 n.samples = 30,
-#                                 n.replicates = 3)
-#
-#   expect_equal(object = dplyr::filter(data.frame(PFM.slow.1$everything), replicate == 1),
-#                expected = dplyr::filter(data.frame(PFM.slow.2$everything), replicate == 1))
-#
-#   expect_equal(object = data.frame(PFM.slow.1$simulated.proxy),
-#                expected = data.frame(PFM.slow.2$simulated.proxy))
-#
-# }
+# mixed layer -----
+
+test_that("mixed layer modelled", {
+  
+  clim.in <- cbind(1:22040)
+  clim.in <- ts(clim.in, start = 1)
+  
+  tpts <- round(seq(10, 5000, length.out = 10))
+  
+  set.seed(26052017)
+  PFM.slow <- ClimToProxyClim(clim.signal = clim.in,
+                              timepoints = tpts,
+                              #habitat.weights = rep(1/12, 12),
+                              sed.acc.rate = rep(2, length(tpts)),
+                              layer.width = 0,
+                              bio.depth = 10,
+                              sigma.meas = 0.46,
+                              sigma.ind = 0,
+                              n.samples = 30,
+                              n.replicates = 1)
+  set.seed(26052017)
+  PFM.fast <- ClimToProxyClim(clim.signal = clim.in,
+                              timepoints = tpts,
+                              #habitat.weights = rep(1/12, 12),
+                              sed.acc.rate = 2,
+                              layer.width = 0,
+                              bio.depth = 10,
+                              sigma.meas = 0.46,
+                              sigma.ind = 0,
+                              n.samples = 30,
+                              n.replicates = 1)
+  
+  
+  expect_equal(object = max(diff(PFM.slow$simulated.proxy$proxy.bt)),
+               expected = 0)
+  
+  expect_equal(object = max(diff(PFM.fast$simulated.proxy$proxy.bt)),
+               expected = 0)
+})
+
+
+
 
